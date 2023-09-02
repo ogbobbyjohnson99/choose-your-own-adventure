3541d2cf3700bbdd2c0bed6b121c9da2ed4bff7a
robert Holmes <bobbyholmes14@gmail.com>
2023-09-01 22:48:13 -0400

    first commit

---
+++ Rholmes choose your own adventure.py
@@ -0,0 +1,5 @@
+print (f"Hello weary traveler. I am going to tell you a story of a high elf and there adventures of danger and peril")
+print(f"Before we get to the story I need to know a couples things about you")
+print(f"press the enter key after typing your answer")
+input(f"Press the enter key")
+
